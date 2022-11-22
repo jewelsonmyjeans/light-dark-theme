7f4077f8fe23894f6d0caa7d763b79ae40fd8417
Aviad Churaman <aviadchuraman@gmail.com>
2022-11-22 15:19:48 -0600

    1

---
+++ index.js
@@ -0,0 +1,10 @@
+// ✨ You got this! 
+function toggle() {
+    const toggle = document.getElementById("header")
+    const toggle2 = document.getElementById("post1")
+    const toggle3 = document.getElementById("post2")
+    const toggle4 = document.getElementsByClassName("body")
+    toggle.classList.toggle("dark")
+    toggle2.classList.toggle("dark")
+    toggle3.classList.toggle("dark")
+}
